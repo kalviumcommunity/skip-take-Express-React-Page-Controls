97c7e745d724883c747ba82ccffbb7fcc5a7311d
Nandan . N <nandan.n.s.123@kalvium.community>
2026-09-21 09:37:24 +0530

    Update threads.js

--- server/routes/threads.js
+++ server/routes/threads.js
@@ -1,19 +1,27 @@
+```js
 import { Router } from "express";
 import prisma from "../prisma/client.js";
 
 const router = Router();
 
+// GET all threads
 router.get("/", async (req, res, next) => {
   try {
     const threads = await prisma.thread.findMany({
-      orderBy: { createdAt: "desc" },
+      orderBy: {
+        createdAt: "desc",
+      },
+
       include: {
+        // Get author information
         author: {
           select: {
             name: true,
             avatarUrl: true,
           },
         },
+
+        // Get the number of comments
         _count: {
           select: {
             comments: true,
@@ -22,10 +30,14 @@ router.get("/", async (req, res, next) => {
       },
     });
 
-    res.json({ threads });
+    res.status(200).json({
+      threads,
+    });
   } catch (error) {
     next(error);
   }
 });
 
 export default router;
+```
+
